dc7dd74f01de8a41f460d9a9014f02b378ffef49
Austin Liu <arl96@cornell.edu>
2016-01-09 22:04:20 -0500

    changing routing to use angular for less repeated code

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-var app = angular.module('main', ['ctrls']);
+var app = angular.module('main', ['ctrls', 'ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider.when("/",
+    {
+      templateUrl: "templates/main.html",
+      controller: "main.ctrl"
+    })
+  .when("/about", 
+  {
+  	templateUrl: "templates/about.html",
+  	controller: "about.ctrl"
+  })
+  .when("/contact", 
+  {
+  	templateUrl: "templates/contact.html",
+  	controller: "contact.ctrl"
+  })
+  .when("/experience",
+  {
+  	templateUrl: "templates/experience.html",
+  	controller: "experience.ctrl"
+  })
+  .when("/projects",
+  {
+  	templateUrl: "templates/projects.html",
+  	controller: "projects.ctrl"
+  });
+});
--- js/controllers.js
+++ js/controllers.js
@@ -1,1 +1,13 @@
 var controllers = angular.module("ctrls", []);
+
+controllers.controller('main.ctrl', ["$scope", function($scope){
+
+}]);
+
+controllers.controller('about.ctrl', ["$scope", function($scope){
+
+}]);
+
+controllers.controller('contact.ctrl', ["$scope", function($scope){
+
+}]);
